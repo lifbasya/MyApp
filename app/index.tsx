6d85b1c72e27dd48cc1f874d69698e573047dc89
lifbasya <alifattallahbasya9>
2025-10-20 19:51:19 +0700

    tugas dua - membuat text dan text input

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,16 @@ export default function Index() {
   return (
     <View>
       {/* belahar text */}
-      <Text>Hello Alif Basya</Text>
+      <Text style={{
+        fontSize: 18,
+        marginHorizontal: 20,
+        marginVertical: 10,
+      }}>Aplikasi data diri saya</Text>
+
+      <Text style={{
+        fontSize: 15,
+        marginHorizontal: 20,
+      }}>Nama:</Text>
 
       {/* belajar text input */}
       <TextInput
@@ -14,9 +23,51 @@ export default function Index() {
           borderWidth: 1,
           padding: 5,
           marginHorizontal: 20,
+          marginVertical: 10,
         }}
-        defaultValue="Name me!"
+        defaultValue="Alif Attallah Basya"
       />
+
+      <Text style={{
+        fontSize: 15,
+        marginHorizontal: 20,
+      }}>Kelas:</Text>
+
+      {/* belajar text input */}
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          padding: 5,
+          marginHorizontal: 20,
+          marginVertical: 10,
+        }}
+        defaultValue="Kelas 11"
+      />
+
+      <Text style={{
+        fontSize: 15,
+        marginHorizontal: 20,
+      }}>Hobi:</Text>
+
+      {/* belajar text input */}
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          padding: 5,
+          marginHorizontal: 20,
+          marginVertical: 10,
+        }}
+        defaultValue="Bermain Sepak Bola"
+      />
+
+      <Text style={{
+        fontSize: 15,
+        marginHorizontal: 20,
+      }}>Terima kasih sudah mengisi data!</Text>
     </View>
   );
 }
