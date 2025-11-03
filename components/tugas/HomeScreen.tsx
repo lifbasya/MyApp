abd542a25ca332410a76423524d6d56ac6fdbcf7
lifbasya <alifattallahbasya9>
2025-11-03 11:34:14 +0700

    menambah empty

--- components/tugas/HomeScreen.tsx
+++ components/tugas/HomeScreen.tsx
@@ -2,6 +2,7 @@ import { Ionicons } from "@expo/vector-icons";
 import {
   FlatList,
   Image,
+  ImageSourcePropType,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -9,23 +10,68 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const notes = [
+const notes: Note[] = [
   {
     id: 1,
+    image: require("@/assets/images/image-0.png"),
     title: "Belajar React Native",
     description: "Mempelajari dasar-dasar React Native",
     date: "2023-10-01",
   },
   {
     id: 2,
+    image: require("@/assets/images/image-1.png"),
     title: "Membuat Aplikasi Catatan",
     description: "Membangun aplikasi catatan sederhana",
     date: "2023-10-02",
+  },
+  {
+    id: 3,
+    image: require("@/assets/images/image-2.png"),
+    title: "Menggunakan Expo",
+    description: "Memahami cara kerja Expo dalam pengembangan aplikasi",
+    date: "2023-10-03",
+  },
+  {
+    id: 4,
+    image: require("@/assets/images/image-3.png"),
+    title: "Deploy Aplikasi",
+    description: "Mempelajari cara deploy aplikasi ke App Store dan Play Store",
+    date: "2023-10-04",
+  },
+  {
+    id: 5,
+    image: require("@/assets/images/image-3.png"),
+    title: "Deploy Aplikasi",
+    description: "Mempelajari cara deploy aplikasi ke App Store dan Play Store",
+    date: "2023-10-04",
+  },
+  {
+    id: 6,
+    image: require("@/assets/images/image-3.png"),
+    title: "Deploy Aplikasi",
+    description: "Mempelajari cara deploy aplikasi ke App Store dan Play Store",
+    date: "2023-10-04",
+  },
+  {
+    id: 7,
+    image: require("@/assets/images/image-3.png"),
+    title: "Deploy Aplikasi",
+    description: "Mempelajari cara deploy aplikasi ke App Store dan Play Store",
+    date: "2023-10-04",
+  },
+  {
+    id: 8,
+    image: require("@/assets/images/image-3.png"),
+    title: "Deploy Aplikasi",
+    description: "Mempelajari cara deploy aplikasi ke App Store dan Play Store",
+    date: "2023-10-04",
   },
 ];
 
 type Note = {
   id: number;
+  image: ImageSourcePropType | { uri: string };
   title: string;
   description: string;
   date: string;
@@ -34,10 +80,7 @@ type Note = {
 const NoteItem = ({ item }: { item: Note }) => {
   return (
     <View style={styles.card}>
-      <Image
-        source={require("@/assets/images/notes-logo.png")}
-        style={{ width: 85, height: 85 }}
-      />
+      <Image source={item.image} style={{ width: 85, height: 85 }} />
       <View style={styles.cardContainer}>
         <View
           style={{
@@ -81,7 +124,8 @@ export default function HomeScreen() {
           data={notes}
           renderItem={({ item }) => <NoteItem item={item} />}
           keyExtractor={(item) => item.id.toString()}
-          contentContainerStyle={{ gap: 5 }}
+          contentContainerStyle={{ gap: 5, flexGrow: 1 }}
+          ListEmptyComponent={() => <EmptyData />}
         />
       </View>
       <TouchableOpacity style={styles.fab}>
@@ -90,6 +134,21 @@ export default function HomeScreen() {
     </SafeAreaView>
   );
 }
+
+const EmptyData = () => {
+  return (
+    <View style={styles.emptyContainer}>
+      <Image
+        source={require("@/assets/images/big-image.png")}
+        style={{ width: 160, height: 160 }}
+      />
+      <Text style={styles.emptyTitle}>Add your first note</Text>
+      <Text style={styles.emptyDesc}>
+        Save your thoughts, tasks or inspirations
+      </Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -148,6 +207,21 @@ const styles = StyleSheet.create({
   cardDate: {
     fontSize: 12,
     opacity: 0.5,
+    fontWeight: 500,
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  emptyTitle: {
+    fontSize: 18,
+    fontWeight: 600,
+  },
+  emptyDesc: {
+    fontSize: 16,
+    opacity: 0.6,
     fontWeight: 500,
   },
 });
