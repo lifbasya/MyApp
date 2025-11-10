4791a4c2a8f75626dcd6b6d622877b4c815c9f81
lifbasya <alifattallahbasya9>
2025-11-10 13:39:54 +0700

    membuat on press

---
+++ app/add/index.tsx
@@ -0,0 +1,153 @@
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const images = [
+  require("@/assets/images/image-0.png"),
+  require("@/assets/images/image-1.png"),
+  require("@/assets/images/image-2.png"),
+  require("@/assets/images/image-3.png"),
+];
+
+export default function AddScreen() {
+  const [selectedImage, setSelectedImage] = useState<number>(0);
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* Bagian atas (AppBar dan form) */}
+      <View style={styles.content}>
+        <View style={styles.appBar}>
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons
+              name="arrow-back"
+              size={24}
+              style={{ color: "#FF5B13" }}
+            />
+          </TouchableOpacity>
+
+          <Text style={styles.appBarTitle}>Add Note</Text>
+        </View>
+
+        <View style={styles.form}>
+          <TextInput
+            style={styles.inputTitle}
+            placeholder="Note Title"
+            selectionColor="#FF5B13"
+          />
+          <TextInput
+            style={styles.inputContent}
+            placeholder="Note Content"
+            selectionColor="#FF5B13"
+            textAlignVertical="top"
+            multiline={true}
+          />
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            style={{ marginTop: 5 }}
+          >
+            {images.map((img, index) => (
+              <TouchableOpacity
+                key={index}
+                style={[
+                  styles.boxImage,
+                  selectedImage === index && styles.imageSelected,
+                ]}
+                onPress={() => setSelectedImage(index)}
+              >
+                <Image style={styles.image} source={img} />
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+      </View>
+
+      {/* Tombol di bawah */}
+      <TouchableOpacity style={styles.addButton}>
+        <Text style={styles.addText}>Add</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  content: {
+    flex: 1, // supaya isi di atas mengisi ruang dan tombol tetap di bawah
+  },
+  appBar: {
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 9,
+    marginVertical: 16,
+  },
+  appBarTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  inputTitle: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 15,
+    marginBottom: 10,
+  },
+  inputContent: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 15,
+    marginBottom: 10,
+    height: 150,
+  },
+  form: {
+    padding: 10,
+  },
+  boxImage: {
+    height: 100,
+    width: 100,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 15,
+    padding: 5,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageSelected: {
+    borderColor: "#FF5B13",
+    borderWidth: 1.5,
+  },
+  addButton: {
+    backgroundColor: "#FF5B13",
+    padding: 12,
+    margin: 10,
+    borderRadius: 5,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  addText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,171 @@
+import { notes } from "@/data/notesdata";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import {
+  FlatList,
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import Latihan1 from "@/components/latihan/Latihan1";
-import Latihan2 from "@/components/latihan/Latihan2";
-import Latihan3 from "@/components/latihan/Latihan3";
-import Latihan4 from "@/components/latihan/Latihan4";
-import AddScreen from "@/components/tugas/addScreen";
-import DetailScreen from "@/components/tugas/detailScreen";
-import HomeScreen from "@/components/tugas/HomeScreen";
-import LoginScreen from "@/components/tugas/LoginScreen";
-import UpdateScreen from "@/components/tugas/updateScreen";
+type Note = {
+  id: number;
+  image: ImageSourcePropType | { uri: string };
+  title: string;
+  description: string;
+  date: string;
+};
 
-
-export default function Index() {
+const NoteItem = ({ item }: { item: Note }) => {
   return (
-    <>
-    {/* <Latihan1 /> */}
-    {/* <LoginScreen /> */}
-    {/* <Latihan2 /> */}
-    {/* <Latihan3 /> */}
-    {/* <Latihan4 /> */}
-    {/* <HomeScreen /> */}
-    {/* <DetailScreen /> */}
-    {/* <AddScreen /> */}
-    <UpdateScreen />
-    </>
+    <View style={styles.card}>
+      <Image source={item.image} style={{ width: 85, height: 85 }} />
+      <View style={styles.cardContainer}>
+        <View
+          style={{
+            flexDirection: "column",
+            gap: 10,
+            justifyContent: "center",
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "column",
+              gap: 4,
+              justifyContent: "center",
+            }}
+          >
+            <Text style={styles.cardTitle}>{item.title}</Text>
+            <Text style={styles.cardDesc}>{item.description}</Text>
+          </View>
+          <Text style={styles.cardDate}>{item.date}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default function HomeScreen() {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require("@/assets/images/notes-logo.png")}
+          style={{ width: 40, height: 40 }}
+        />
+        <View style={{ gap: 4, flexDirection: "row", alignItems: "center" }}>
+          <Text style={styles.kodeinteks}>Kodein</Text>
+          <Text style={styles.notesteks}>Notes</Text>
+        </View>
+      </View>
+      <View style={styles.content}>
+        <FlatList
+          data={notes}
+          renderItem={({ item }) => <NoteItem item={item} />}
+          keyExtractor={(item) => item.id.toString()}
+          contentContainerStyle={{ gap: 5, flexGrow: 1 }}
+          ListEmptyComponent={() => <EmptyData />}
+        />
+      </View>
+      <TouchableOpacity style={styles.fab} onPress={() => router.push("/add")}>
+        <Ionicons name="add" size={32} style={{ color: "white" }} />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
+
+const EmptyData = () => {
+  return (
+    <View style={styles.emptyContainer}>
+      <Image
+        source={require("@/assets/images/big-image.png")}
+        style={{ width: 160, height: 160 }}
+      />
+      <Text style={styles.emptyTitle}>Add your first note</Text>
+      <Text style={styles.emptyDesc}>
+        Save your thoughts, tasks or inspirations
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    padding: 10,
+  },
+  content: {
+    flex: 1,
+  },
+  kodeinteks: {
+    fontSize: 20,
+    fontWeight: 700,
+  },
+  notesteks: {
+    fontSize: 20,
+    fontWeight: 700,
+    color: "#FF5B13",
+  },
+  fab: {
+    backgroundColor: "#FF5B13",
+    width: 60,
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    position: "absolute",
+    right: 25,
+    bottom: 60,
+  },
+  card: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    marginHorizontal: 10,
+    marginVertical: 5,
+    borderRadius: 10,
+    borderColor: "#000000ff",
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  cardContainer: {
+    flex: 1,
+  },
+  cardTitle: {
+    fontSize: 16,
+    fontWeight: 500,
+  },
+  cardDesc: {},
+  cardDate: {
+    fontSize: 12,
+    opacity: 0.5,
+    fontWeight: 500,
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  emptyTitle: {
+    fontSize: 18,
+    fontWeight: 600,
+  },
+  emptyDesc: {
+    fontSize: 16,
+    opacity: 0.6,
+    fontWeight: 500,
+  },
+});
